99af199029aaa5ce8d8db67f9e9246cdf4efc18a
paryanmousavi <paryanmosavi@gmail.com>
2025-11-07 11:44:10 -0800

    Add files via upload

---
+++ example1(4)/example1(4)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example1_4_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int evenCount = 0;
+            int oddCount = 0;
+            Console.WriteLine("plese enter 10 numbers:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"number{i}:");
+                int number = int.Parse(Console.ReadLine());
+                if (number % 2 == 0)
+                    ++evenCount;
+                else
+                    ++oddCount;
+                {
+                    Console.WriteLine("------------------------------------------------");
+                    Console.WriteLine($"number of even numbers:{evenCount}");
+                    Console.WriteLine($"number of odd numbers:{oddCount}");
+
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ example2(4)/example2(4)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example2_4_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {int position = -1;
+            Console.WriteLine("plese enter 5 numbers:");
+            for(int i = 1; i<=5; i++)
+            {
+                Console.Write($"number{i}:");
+                int number = int.Parse(Console.ReadLine());
+                if (number == 2)
+                {
+                    position = i;
+                    break;
+                }
+            }
+            }
+    }
+}
