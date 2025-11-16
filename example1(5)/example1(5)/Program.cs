ae292d472fc22901d14594ec495d926d4771088c
paryanmousavi <paryanmosavi@gmail.com>
2025-11-16 12:28:09 -0800

    Add files via upload

---
+++ example1(5)/example1(5)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example1_5_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.Write("enter saide a:");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("enter saide b :");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("enter saide c :");
+            c = double.Parse(Console.ReadLine());
+            double perimeter = a + b + c;
+            double s = perimeter / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            Console.WriteLine("perimeter:" + perimeter);
+            Console.WriteLine("Area:" + area);
+
+
+
+            {
+                
+            }
+
+
+
+        }
+    }
+}
---
+++ example2(5)/example2(5)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example2_5_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int Multiplay(int a, int b)
+            {
+                if (b == 0)
+                    return 0;
+                return a + Multiplay(a, b - 1);
+                {
+                    
+                        {
+                        Console.Write(" enter the ferst number :");
+                        int.Parse(Console.ReadLine ());
+                        int numb1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("enter the second number:");
+                        int num2 = int.Parse(Console.ReadLine());
+                        int result = Multiplay(numb1, num2);
+                        Console.WriteLine("the result of multiplication is :+ result");
+                    }
+                }
+            }
+
+        }
+
+    }
+}
+    
